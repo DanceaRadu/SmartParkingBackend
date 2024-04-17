a20303952769460e87f3d4c2ccb53424b97523ae
DanceaRadu <radu.dancea@gmail.com>
2024-04-17 10:36:19 +0300

    #9 Added interceptor.

---
+++ src/main/kotlin/com/embeddedsystems/smartparkingbackend/config/CustomHandshakeInterceptor.kt
@@ -0,0 +1,32 @@
+package com.embeddedsystems.smartparkingbackend.config
+
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.WebSocketHandler
+import org.springframework.web.socket.server.HandshakeInterceptor
+
+@Component
+class CustomHandshakeInterceptor : HandshakeInterceptor {
+
+    override fun beforeHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        attributes: MutableMap<String, Any>
+    ): Boolean {
+        response.setStatusCode(HttpStatusCode.valueOf(101))
+        return true
+    }
+
+    override fun afterHandshake(
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+        wsHandler: WebSocketHandler,
+        exception: Exception?
+    ) {
+        // Do nothing after handshake
+    }
+}
+
--- src/main/kotlin/com/embeddedsystems/smartparkingbackend/config/WebSocketConfig.kt
+++ src/main/kotlin/com/embeddedsystems/smartparkingbackend/config/WebSocketConfig.kt
@@ -5,12 +5,18 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler
 
 @EnableWebSocketMessageBroker
 @Configuration
 class WebSocketConfig: WebSocketMessageBrokerConfigurer {
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
-        registry.addEndpoint("/config").setAllowedOriginPatterns("*").withSockJS()
+        registry
+            .addEndpoint("/config")
+            .setAllowedOriginPatterns("*")
+            .setHandshakeHandler(DefaultHandshakeHandler())
+            .addInterceptors(CustomHandshakeInterceptor())
+            .withSockJS()
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
